c5c002b45dc9e30f1fc40bb6a9e642f56c440360
trofkm <trofkm@gmail.com>
2023-09-28 18:29:37 +0300

    feat: Add user input mode and automatic test generation.

--- Lab1/1_1_char_list.cpp
+++ Lab1/1_1_char_list.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <iostream>
 
+#include "utils/generator.h"
 #include "utils/utils.h"
 
 struct ListNode {
@@ -93,7 +94,35 @@ void business_logic(char *input_a, char *input_b, char *input_c, char *input_d, 
     char_list_to_cstring(&result, output_str);
 }
 
-int main() {
+
+void run_tests(int count) {
+    double average_time = 0;
+    for (int i = 0; i < count; i++) {
+        auto input_a = utils::generate_char_set(10);
+        auto input_b = utils::generate_char_set(10);
+        auto input_c = utils::generate_char_set(10);
+        auto input_d = utils::generate_char_set(5);
+
+        char result_str[80] = {0};
+        auto elapsed = utils::measure_execution_time(business_logic, input_a, input_b, input_c, input_d, result_str);
+        std::cout << "----------------------\n";
+        std::cout << "Case " << i << ":\n";
+        std::cout << "A=" << input_a << "\n";
+        std::cout << "B=" << input_b << "\n";
+        std::cout << "C=" << input_c << "\n";
+        std::cout << "D=" << input_d << "\n";
+        std::cout << "E=" << result_str << "\n";
+        std::cout << "Execution time: " << std::setprecision(10) << elapsed << "ms" << std::endl;
+        delete[] input_a;
+        delete[] input_b;
+        delete[] input_c;
+        delete[] input_d;
+        average_time += elapsed / count;
+    }
+    std::cout << "======================\n";
+    std::cout << "Average time: " << std::setprecision(10) << average_time << "ms" << std::endl;
+}
+void user_input() {
     char input_a[80];
     char input_b[80];
     char input_c[80];
@@ -113,5 +142,25 @@ int main() {
     auto elapsed = utils::measure_execution_time(business_logic, input_a, input_b, input_c, input_d, result_str);
     std::cout << "E=" << result_str << "\n";
     std::cout << "Execution time: " << std::setprecision(10) << elapsed << "ms\n";
+}
+
+
+int main() {
+    std::cout << "Select mode: 1 - user input, 2 - random: ";
+
+    int mode = 1;
+    std::cin >> mode;
+    switch (mode) {
+        case 1:
+            user_input();
+            break;
+        case 2:
+            std::cout << "Enter number of tests: ";
+            int count;
+            std::cin >> count;
+            std::cout << "Running tests..." << std::endl;
+            run_tests(count);
+            break;
+    }
     return 0;
 }
