91d1fb564b143e377b81434ca0c67006ef527bd9
referenceCat <mi.buyanov@yandex.ru>
2023-10-06 12:19:51 +0300

    feat: cmake structure

---
+++ Lab2/tests/main_lab2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by referencecat on 06.10.23.
+//
+
+#include "modules/BitArraySet.h"
+#include "modules/CharArraySet.h"
+#include "modules/CharListSet.h"
+#include "modules/MachineWordSet.h"
+
+int main() {
+    return 0;
+}
