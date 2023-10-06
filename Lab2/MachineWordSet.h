dbf000d37071350ede2865f4ed9dde3eeb79283f
referenceCat <mi.buyanov@yandex.ru>
2023-10-06 12:05:01 +0300

    feat: lab2 init

---
+++ Lab2/MachineWordSet.h
@@ -0,0 +1,14 @@
+//
+// Created by referencecat on 06.10.23.
+//
+
+#ifndef A_DS_REMOTE_MACHINEWORDSET_H
+#define A_DS_REMOTE_MACHINEWORDSET_H
+
+
+class MachineWordSet {
+
+};
+
+
+#endif //A_DS_REMOTE_MACHINEWORDSET_H
