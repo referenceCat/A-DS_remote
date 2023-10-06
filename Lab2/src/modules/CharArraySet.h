4bfd69a459a97e0f8da65a2c2a94a68f4185a7af
referenceCat <mi.buyanov@yandex.ru>
2023-10-06 12:41:21 +0300

    feat: class templates

--- Lab2/src/modules/CharArraySet.h
+++ Lab2/src/modules/CharArraySet.h
@@ -7,7 +7,22 @@
 
 
 class CharArraySet {
-
+private: // Закрытая часть класса — данные
+    static int N, cnt; // мощность универсума и счётчик множеств
+    int n;// мощность множества
+    char S;
+    char *char_array; // тег и память для множества
+public: // Открытая часть — функции для работы с множеством
+    CharArraySet operator|(const CharArraySet &) const; // объединение
+    CharArraySet operator&(const CharArraySet &) const; // пересечение
+    CharArraySet operator~() const; // дополнение до универсума
+    void Show(); // вывод множества на экран
+    int power() { return n; } // получение мощности
+    CharArraySet(char); // конструктор множества
+    CharArraySet(); // ещё конструктор — по умолчанию
+    CharArraySet(const CharArraySet &); // конструктор копии
+    CharArraySet operator=(const CharArraySet &); // оператор присваивания
+    ~CharArraySet() { delete[] char_array; } // деструкторконстру
 };
 
 
