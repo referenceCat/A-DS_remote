e08bcd91be7667329991d4147902f0d5c8baf0e4
referenceCat <mi.buyanov@yandex.ru>
2023-10-06 12:05:01 +0300

    feat: lab2 init

---
+++ Lab2/BitArraySet.h
@@ -0,0 +1,14 @@
+//
+// Created by referencecat on 06.10.23.
+//
+
+#ifndef A_DS_REMOTE_BITARRAYSET_H
+#define A_DS_REMOTE_BITARRAYSET_H
+
+
+class BitArraySet {
+
+};
+
+
+#endif //A_DS_REMOTE_BITARRAYSET_H
---
+++ Lab2/CharListSet.h
@@ -0,0 +1,14 @@
+//
+// Created by referencecat on 06.10.23.
+//
+
+#ifndef A_DS_REMOTE_CHARLISTSET_H
+#define A_DS_REMOTE_CHARLISTSET_H
+
+
+class CharListSet {
+
+};
+
+
+#endif //A_DS_REMOTE_CHARLISTSET_H
