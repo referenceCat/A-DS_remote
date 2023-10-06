211e92b6ce18e755d31a65ed070b7bf3fad2c7aa
referenceCat <mi.buyanov@yandex.ru>
2023-10-06 12:41:21 +0300

    feat: class templates

--- Lab2/src/modules/CharListSet.h
+++ Lab2/src/modules/CharListSet.h
@@ -7,7 +7,26 @@
 
 
 class CharListSet {
-
+private:
+    struct Node {
+        Node *next;
+        char value;
+    };// Закрытая часть класса — данные
+    static int N, cnt; // мощность универсума и счётчик множеств
+    int n;// мощность множества
+    char S;
+    Node *first; // тег и память для множества
+public: // Открытая часть — функции для работы с множеством
+    CharListSet operator|(const CharListSet &) const; // объединение
+    CharListSet operator&(const CharListSet &) const; // пересечение
+    CharListSet operator~() const; // дополнение до универсума
+    void Show(); // вывод множества на экран
+    int power() { return n; } // получение мощности
+    CharListSet(char); // конструктор множества
+    CharListSet(); // ещё конструктор — по умолчанию
+    CharListSet(const CharListSet &); // конструктор копии
+    CharListSet operator=(const CharListSet &); // оператор присваивания
+    ~CharListSet(); // деструкторконстру
 };
 
 
--- Lab2/src/modules/MachineWordSet.h
+++ Lab2/src/modules/MachineWordSet.h
@@ -7,7 +7,22 @@
 
 
 class MachineWordSet {
-
+private:
+    static int N, cnt; // мощность универсума и счётчик множеств
+    int n;// мощность множества
+    char S;
+    unsigned short data;
+public: // Открытая часть — функции для работы с множеством
+    MachineWordSet operator|(const MachineWordSet &) const; // объединение
+    MachineWordSet operator&(const MachineWordSet &) const; // пересечение
+    MachineWordSet operator~() const; // дополнение до универсума
+    void Show(); // вывод множества на экран
+    int power() { return n; } // получение мощности
+    MachineWordSet(char); // конструктор множества
+    MachineWordSet(); // ещё конструктор — по умолчанию
+    MachineWordSet(const MachineWordSet &); // конструктор копии
+    MachineWordSet operator=(const MachineWordSet &); // оператор присваивания
+    ~MachineWordSet(); // деструкторконстру
 };
 
 
