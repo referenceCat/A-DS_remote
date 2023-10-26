c64f2e00dc42ac369fcbce67620d28232a9402ce
Kirill Trofimov <trofkm@gmail.com>
2023-10-26 20:50:47 +0300

    feat: Add char array set implementation.

--- Lab2/src/modules/CharArraySet.h
+++ Lab2/src/modules/CharArraySet.h
@@ -8,25 +8,28 @@
 #include <iostream>
 
 class CharArraySet {
-private: // Закрытая часть класса — данные
-    static int N, cnt; // мощность универсума и счётчик множеств
-    int n;// мощность множества
+private:              // Закрытая часть класса — данные
+    static int N, cnt;// мощность универсума и счётчик множеств
+    int n;            // мощность множества
     char S;
-    char *char_array; // тег и память для множества
-public: // Открытая часть — функции для работы с множеством
-    CharArraySet operator|(const CharArraySet &) const; // объединение
-    CharArraySet operator&(const CharArraySet &) const; // пересечение
-    CharArraySet operator~() const; // дополнение до универсума
-    void Show(); // вывод множества на экран
-    int power() { return n; } // получение мощности
-    CharArraySet(char); // конструктор множества
-    CharArraySet(); // ещё конструктор — по умолчанию
-    CharArraySet(const CharArraySet &); // конструктор копии
-    CharArraySet operator=(const CharArraySet &); // оператор присваивания
-    ~CharArraySet() { delete[] char_array; } // деструкторконстру
+    char *char_array;                                       // тег и память для множества
+public:                                                     // Открытая часть — функции для работы с множеством
+    CharArraySet operator|(const CharArraySet &other) const;// объединение
+    CharArraySet operator|=(const CharArraySet &other);     // объединение с присваиванием
+    CharArraySet operator&(const CharArraySet &other) const;// пересечение
+    CharArraySet operator&=(const CharArraySet &other);     // пересечение с присваиванием
+    CharArraySet operator~() const;                         // дополнение до универсума
+    int power() const { return n; }                         // получение мощности
+    explicit CharArraySet(char);                            // конструктор множества
+    CharArraySet();                                         // ещё конструктор — по умолчанию
+    CharArraySet(const CharArraySet &other);                // конструктор копии
+    CharArraySet &operator=(const CharArraySet &other);     // оператор присваивания
+    CharArraySet &operator=(CharArraySet &&other) noexcept;
+    CharArraySet(CharArraySet &&other) noexcept;
+    ~CharArraySet() { delete[] char_array; }// деструкторконстру
 
-    friend std::ostream &operator<<(std::ostream &, const CharArraySet &); // вывод множества на экран
+    friend std::ostream &operator<<(std::ostream &, const CharArraySet &other);// вывод множества на экран
 };
 
 
-#endif //A_DS_REMOTE_CHARARRAYSET_H
+#endif//A_DS_REMOTE_CHARARRAYSET_H
